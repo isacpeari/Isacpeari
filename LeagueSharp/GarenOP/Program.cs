abc3c67e5c674633036e7c99181e857b15ea9704
isacpeari <dualblazingblade@hotmail.com>
2014-11-17 16:25:48 +0800

    Update Program.cs

--- LeagueSharp/GarenOP/Program.cs
+++ LeagueSharp/GarenOP/Program.cs
@@ -1,4 +1,5 @@
-using System;
+
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -199,17 +200,3 @@ catch (Exception e)
 }
 }
 }
-
-    Status
-    API
-    Training
-    Shop
-    Blog
-    About
-
-    © 2014 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Contact
-
